a6f2edd7e951e1283971c70580ba0a630846989d
arseneaultt2 <arseneaultt2@mymacewan.ca>
2018-05-11 11:53:39 -0600

    Initial commit

---
+++ Assets/AIController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *
+ * Author: Tyler Arseneault
+ * Due:
+ * Description: Controller for the follower jets.
+ * The follower jets will follow specific points attached to
+ * the lead jet. Their speed will increase and decrease with the lead jet.
+ *
+ */
+public class AIController : MonoBehaviour {
+
+	public Transform target; //transform of the target gameobject
+	private Transform transform; //transform of the AI
+	private Rigidbody rbody; //rigidbody of the AI
+
+	/**
+	 * Initializes important components for the AI
+	 */
+	void Start () {
+		rbody = GetComponent<Rigidbody> ();
+		transform = GetComponent<Transform> ();
+	}
+
+	/**
+	 * Updates the AI every frame
+	 */
+	void Update () {
+
+		//ensures that the AI remains at the same speed as the lead jet
+		float speed = target.GetComponentInParent<JetController> ().speed;
+		rbody.velocity = transform.forward * speed;
+
+		//calculate the location of the target compared to the AI
+		//move the AI in the direction of the target
+		Vector3 location = target.position - transform.position;
+		Vector3 heading = rbody.velocity;
+		rbody.velocity = Vector3.RotateTowards (heading, location, 60f, 0f);
+		transform.LookAt (target); //always look at the target
+
+	}
+}
---
+++ Assets/Scripts/JetController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Author: Tyler Arseneault
+ * Due:
+ * Description: Controller for the player controlled Jet.
+ * This allows the player to control the pitch, yaw, and speed of the jet.
+ */
+public class JetController : MonoBehaviour {
+
+	private Rigidbody rbody; //rigidbody of the jet
+	private Transform transform; //transform of the jet
+	public float rotateSpeed; //how fast the jet rotates on either axis for pitch and yaw
+
+	public float speed; //speed of the jet
+
+	/**
+	 * Initializes some of the important components of the jet for use
+	 */
+	void Start () {
+		rbody = GetComponent<Rigidbody> ();
+		transform = GetComponent<Transform> ();
+	}
+	
+	/**
+	 * Updates the jet every frame
+	 */
+	void Update () {
+
+		Movement ();
+
+	}
+
+	/**
+	 * Controls the movements of the jet through the air
+	 */
+	void Movement(){
+
+		AdjustSpeed ();
+
+		rbody.velocity = transform.forward * speed;
+
+		AdjustRotation ();
+
+	}
+
+	/**
+	 * Changes the speed of the jet when the scroll wheel is moved up or down
+	 */
+	void AdjustSpeed(){
+
+		speed += Input.GetAxis ("Mouse ScrollWheel");
+		if (speed < 10) {
+			speed = 10;
+		} else if (speed > 50) {
+			speed = 50;
+		}
+
+	}
+
+	/**
+	 * Alters the direction of the jet based on key presses
+	 */
+	void AdjustRotation(){
+	
+		if (Input.GetKey (KeyCode.D))
+			transform.Rotate (Vector3.up * rotateSpeed * Time.deltaTime);
+		else if (Input.GetKey (KeyCode.A))
+			transform.Rotate (Vector3.down * rotateSpeed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.W))
+			transform.Rotate (Vector3.right * rotateSpeed * Time.deltaTime);
+		else if (Input.GetKey (KeyCode.S))
+			transform.Rotate (Vector3.left * rotateSpeed * Time.deltaTime);
+	
+	}
+}
